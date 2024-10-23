ad14eac4bc38cbd515bf34bd0fd5bee4fd49235b
AdityaDaniel06 <danieladitya327@gmail.com>
2024-10-23 13:29:10 +0530

    view toggle

--- frontend/src/pages/Display.jsx
+++ frontend/src/pages/Display.jsx
@@ -1,5 +1,8 @@
 import "./Display.css";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -9,6 +12,7 @@ import { message } from "antd";
 const Display = () => {
   const navigate = useNavigate(); // navigate to edit with id params
   const [booksData, setBooksData] = useState([]); //useState to display all books
+  const [toggleView, setToggleView] = useState(true);
   // get all books
   const fetchData = async () => {
     try {
@@ -65,7 +69,7 @@ const Display = () => {
           <Button
             variant="outline-success"
             size="sm"
-            className="me-3"
+            className="me-2"
             onClick={() => handleEdit(value._id)}
           >
             Edit
@@ -73,7 +77,7 @@ const Display = () => {
           <Button
             variant="outline-danger"
             size="sm"
-            className="me-3"
+            className="mx-1"
             onClick={() => handleDelete(value._id)}
           >
             Delete
@@ -83,27 +87,83 @@ const Display = () => {
     );
   });
 
+  const cardResponse = booksData.map((value) => {
+    return (
+      <Card
+        border="success"
+        style={{ width: "15rem" }}
+        key={value?.id}
+        className="p-0 m-2"
+      >
+        <Card.Img
+          variant="top"
+          src={`https://picsum.photos/seed/${value?._id}/300/200`}
+          className="p-0"
+        />
+        <Card.Body className="p-2">
+          <Card.Title>{value.title}</Card.Title>
+          <Card.Text className="m-0">Author: {value.author || ""}</Card.Text>
+          <Card.Text className="m-0">Genre: {value.genre}</Card.Text>
+          <Card.Text className="m-0">Price: ₹{value.price}</Card.Text>
+          <Card.Text className="m-0">
+            Edition: {value.publicationYear}
+          </Card.Text>
+          <Card.Text className="m-0">Pages: ₹{value.pages}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  });
+
   return (
     <>
-      <div className="w-75 mx-auto mt-3">
-        <h2 style={{ color: "#198754" }}>List of all the books</h2>
-        <table className="w-100 mt-4">
-          <thead>
-            <tr>
-              <th>Book Id</th>
-              <th>Book Title</th>
-              <th>Author</th>
-              <th>Year </th>
-              <th>Pages</th>
-              <th>Genre</th>
-              <th>Price</th>
-              <th>Read</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>{responseData}</tbody>
-        </table>
-      </div>
+      {toggleView ? (
+        <div className="w-75 mx-auto mt-3">
+          <div className="d-flex justify-content-between">
+            <h2 style={{ color: "#198754" }}>List of all the books</h2>
+            <span>
+              <Button
+                variant="success"
+                onClick={() => setToggleView(!toggleView)}
+              >
+                Toggle View
+              </Button>
+            </span>
+          </div>
+          <table className="w-100 mt-4">
+            <thead>
+              <tr>
+                <th>Book Id</th>
+                <th>Book Title</th>
+                <th>Author</th>
+                <th>Year </th>
+                <th>Pages</th>
+                <th>Genre</th>
+                <th>Price</th>
+                <th>Read</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>{responseData}</tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="w-75 mx-auto mt-3 ">
+          <div className="d-flex justify-content-between">
+            <h2 style={{ color: "#198754" }}>List of all the books</h2>
+            <span>
+              <Button
+                variant="success"
+                onClick={() => setToggleView(!toggleView)}
+              >
+                Toggle View
+              </Button>
+            </span>
+          </div>
+          <div className="d-flex justify-content-between m-5">
+            <Row className="m-2">{cardResponse}</Row>
+          </div>
+        </div>
+      )}
     </>
   );
 };
